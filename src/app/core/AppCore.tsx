20ae6cdbf111032d9a020660c13a15f319425089
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-08-06 13:17:24 +0900

    update funcs

--- src/app/core/AppCore.tsx
+++ src/app/core/AppCore.tsx
@@ -1,10 +1,14 @@
 import React, {FC} from 'react';
 import {StoreProvider} from '../store';
+import {AppNavigation} from '../navigation';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const AppCore: FC = () => {
   return (
     <StoreProvider>
-      <></>
+      <SafeAreaProvider>
+        <AppNavigation />
+      </SafeAreaProvider>
     </StoreProvider>
   );
 };
--- src/app/navigation/stacks/rootStack/RootStack.tsx
+++ src/app/navigation/stacks/rootStack/RootStack.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import {ROOT_STACK} from './RootStack.screens';
 import {NO_HEADER_SCREEN_OPTIONS} from '../../constants';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import App from '../../../../../App';
 
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
   return (
     <Stack.Navigator initialRouteName={ROOT_STACK.AUTHENTICATION}>
-      <Stack.Screen name={ROOT_STACK.AUTHENTICATION} component={} options={{...NO_HEADER_SCREEN_OPTIONS}} />
+      <Stack.Screen name={ROOT_STACK.AUTHENTICATION} component={App} options={{...NO_HEADER_SCREEN_OPTIONS}} />
       {/*<Stack.Screen*/}
       {/*  name={ROOT_STACK.RENT_AVAILABLE_MILEAGE}*/}
       {/*  component={RentAvailableMileage}*/}
