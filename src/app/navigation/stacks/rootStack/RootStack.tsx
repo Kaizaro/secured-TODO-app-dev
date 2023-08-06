7c6597770c09cff4320100b8130f615416393c9e
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-08-06 13:13:09 +0900

    update funcs

--- src/app/navigation/stacks/rootStack/RootStack.tsx
+++ src/app/navigation/stacks/rootStack/RootStack.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import {ROOT_STACK} from './RootStack.screens';
 import {NO_HEADER_SCREEN_OPTIONS} from '../../constants';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
   return (
     <Stack.Navigator initialRouteName={ROOT_STACK.AUTHENTICATION}>
-      <Stack.Screen name={ROOT_STACK.AUTHENTICATION} component={SplashScreen} options={{...NO_HEADER_SCREEN_OPTIONS}} />
+      <Stack.Screen name={ROOT_STACK.AUTHENTICATION} component={} options={{...NO_HEADER_SCREEN_OPTIONS}} />
       {/*<Stack.Screen*/}
       {/*  name={ROOT_STACK.RENT_AVAILABLE_MILEAGE}*/}
       {/*  component={RentAvailableMileage}*/}
--- src/app/navigation/utils/getHeader.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import {Header, IHeader} from '@app/navigation/ui/header';
-
-export const getHeader = (props: IHeader) => <Header {...props} />;
