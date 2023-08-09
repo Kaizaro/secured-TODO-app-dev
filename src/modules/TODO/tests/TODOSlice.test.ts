be0f38bf6383f5fbc3821038af06eabd2b8ebf9b
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-08-09 22:56:39 +0900

    add tests for slice

---
+++ src/modules/TODO/tests/TODOSlice.test.ts
@@ -0,0 +1,52 @@
+import {ITODO} from '../entities';
+import {TODOSlice, TODOSliceActions, TODOSliceInitialState} from '../DAL';
+import uuid from 'react-native-uuid';
+import dayjs from 'dayjs';
+import {renderHook} from '@testing-library/react-hooks';
+import {useTODOList} from '../hooks';
+
+const testTODO = {
+  id: uuid.v4().toString(),
+  text: 'test-text',
+  updatedAt: dayjs().valueOf().toString(),
+  timestamp: dayjs().valueOf(),
+  isSelected: false,
+} as ITODO;
+
+describe('initialize TODO reducer', () => {
+  it('get the initial state', () => {
+    expect(TODOSlice.reducer(undefined, {})).toEqual(TODOSliceInitialState);
+  });
+
+  it('handles add item to TODO list', () => {
+    expect(TODOSlice.reducer(TODOSliceInitialState, TODOSliceActions.setNotesList([testTODO]))).toEqual({
+      ...TODOSliceInitialState,
+      TODOList: [testTODO],
+    });
+  });
+
+  it('handles set item as selected from TODO list', () => {
+    expect(TODOSlice.reducer(TODOSliceInitialState, TODOSliceActions.setSelectedNote(testTODO))).toEqual({
+      ...TODOSliceInitialState,
+      selectedTODO: testTODO,
+    });
+  });
+
+  it('handles removing selected item from TODO list', () => {
+    expect(TODOSlice.reducer(TODOSliceInitialState, TODOSliceActions.clearSelectedNote())).toEqual({
+      ...TODOSliceInitialState,
+      selectedTODO: null,
+    });
+  });
+
+  it('handles clear TODO list', () => {
+    expect(TODOSlice.reducer(TODOSliceInitialState, TODOSliceActions.clearNotesList())).toEqual({
+      ...TODOSliceInitialState,
+      TODOList: [],
+    });
+  });
+
+  it('handles clear all reducer', () => {
+    expect(TODOSlice.reducer(TODOSliceInitialState, TODOSliceActions.clearAll())).toEqual(TODOSliceInitialState);
+  });
+});
